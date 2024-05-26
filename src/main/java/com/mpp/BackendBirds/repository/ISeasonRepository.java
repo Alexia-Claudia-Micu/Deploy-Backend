075013f5268656fa83af5202184cc99a1a543d59
Alexia-Claudia Micu <163188334+Alexia-Claudia-Micu@users.noreply.github.com>
2024-05-26 16:31:50 +0300

    Delete src directory

--- src/main/java/com/mpp/BackendBirds/repository/ISeasonRepository.java
+++
@@ -1,8 +0,0 @@
-package com.mpp.BackendBirds.repository;
-
-import com.mpp.BackendBirds.model.Season;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ISeasonRepository extends JpaRepository<Season, Integer> { }
